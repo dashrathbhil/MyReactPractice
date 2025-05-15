4124189fff6a0b0bd5898fba5c075a0d158df287
dashrath24 <dashrathbhil24@gmail.com>
2025-05-15 10:17:43 +0530

    data

---
+++ MovieShow/movieshow/src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import "./index.css";
+import { RouterProvider } from "react-router";
+
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+ 
+      <RouterProvider router={router} />
+  
+  </StrictMode>
+);
