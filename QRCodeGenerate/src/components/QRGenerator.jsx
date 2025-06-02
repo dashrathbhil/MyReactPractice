15c6777ebbdaf778a22e6d222de80655abf6e23c
dashrath24 <dashrathbhil24@gmail.com>
2025-06-02 12:30:24 +0530

    jsx component

--- QRCodeGenerate/src/components/QRGenerator.jsx
+++ QRCodeGenerate/src/components/QRGenerator.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
+import QRCode from "react-qr-code";
+function QRGenerator  () {
+  const [qrCode, setQrCode] = useState("");
+  const [input, setInput] = useState("");
 
-function QRGenerator() {
+  function handleGenerateQrCode() {
+    setQrCode(input);
+    setInput("");
+  }
+
+
   return (
-    <div>QRGenerator</div>
-  )
+    <>
+      <h1>QR Code Generator</h1>
+      <input
+        type="text"
+        onChange={(e) => setInput(e.target.value)}
+        name="qr-code"
+        value={input}
+        placeholder="Enter Something"
+      />
+      <button
+        disabled={input && input.trim() !== "" ? false : true}
+        onClick={handleGenerateQrCode}
+      >
+        Generate
+      </button>
+      <div>
+        <QRCode id="qr-code-value" value={qrCode} size={400} bgColor="#ffff" />
+      </div>
+    </>
+  );
 }
 
-export default QRGenerator
+export default QRGenerator;
