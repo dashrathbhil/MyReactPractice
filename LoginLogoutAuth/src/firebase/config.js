fa52d2e7cc6cfed6e17bc44ee8239df793ac34a9
dashrath24 <dashrathbhil24@gmail.com>
2025-06-05 10:36:26 +0530

    ui change

--- LoginLogoutAuth/src/firebase/config.js
+++ LoginLogoutAuth/src/firebase/config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth,GoogleAuthProvider,FacebookAuthProvider ,GithubAuthProvider} from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC65J6csYYNsSPIb-X__q1Qu1lZdKR5RNQ",
-  authDomain: "auth-fb73f.firebaseapp.com",
-  projectId: "auth-fb73f",
-  storageBucket: "auth-fb73f.firebasestorage.app",
-  messagingSenderId: "162103005881",
-  appId: "1:162103005881:web:4d8cf81580885667528168",
-  measurementId: "G-WRZG999YGN"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
